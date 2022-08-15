845134c685c4e8ba8b2cadaac7d8382582776c02
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-16 01:42:07 +0700

    Update

---
+++ ICPC0104.py
@@ -0,0 +1,18 @@
+from ctypes.wintypes import CHAR
+
+n = int(input())
+
+while n > 0:
+    s = input()
+    s += "*"
+    res = 10 ** 19
+    x = 0
+    l = len(s)
+    for i in range(0, l - 1):
+        if s[i].isdigit():
+            x = x * 10 + int(s[i])
+            if s[i + 1].isalpha():
+                res = min(res, x)
+                x = 0
+    print(res)
+    n -= 1

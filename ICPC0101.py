42aecc90be726802703b61b76ef31706717a35ec
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-16 00:52:56 +0700

    Update

--- ICPC0101.py
+++ ICPC0101.py
@@ -1,18 +1,17 @@
+from operator import le
 from typing import List
 
 n = int(input())
-a = []
-a = (input().split())
+a = [int (i) for i in input().split()]
 res = []
 
 for i in a:
-    x = int(i)
     if len(res) == 0:
-        res.append(x)
+        res.append(i)
     else:
-        if(res[-1] + x) % 2 == 0:
+        if (res[-1] + i) % 2 == 0:
             res.pop()
         else:
-            res.append(x)
+            res.append(i)
 
 print(len(res))

5a02a208e9120b6c8a6c701efdf5ded6b4869a15
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-17 16:33:31 +0700

    Update

---
+++ PY01009.py
@@ -0,0 +1,17 @@
+from curses.ascii import isupper
+
+
+s = input()
+
+upper = 0
+
+for i in range(0, len(s)):
+    if s[i].isupper():
+        upper += 1
+
+lower = len(s) - upper
+
+if(lower >= upper):
+    print(s.lower())
+else:
+    print(s.upper())

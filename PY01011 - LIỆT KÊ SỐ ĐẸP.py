a237747d55e523319fb242f9f943aabf1838cf6b
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-08-24 00:33:44 +0700

    Update

---
+++ PY01011 - LIỆT KÊ SỐ ĐẸP.py
@@ -0,0 +1,25 @@
+t = int(input())
+
+a = []
+
+def check(n):
+    for i in n:
+        if int(i) % 2 == 1:
+            return False
+    return True
+
+num = 2;
+while num <= 888:
+    if check(str(num)):
+        tmp = str(num)
+        a.append(int(tmp + tmp[::-1]))
+    num += 2
+
+while t > 0:
+    n = int(input())
+    for i in a:
+        if i >= n:
+            break
+        print(i, end = ' ')
+    print()
+    t -= 1

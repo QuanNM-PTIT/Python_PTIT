ea15c09804ec5192a2026f87c970730e416c8326
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-10-26 14:27:06 +0700

    Update

---
+++ PY01052 - TỔNG CHỮ SỐ NGUYÊN TỐ.py
@@ -0,0 +1,24 @@
+import math
+
+def isPrime(n):
+    if n < 2:
+        return False
+    if n < 4:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    sqr = int(math.sqrt(n))
+    for i in range (5, sqr + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    return True
+
+for i in range(int(input())):
+    n = input()
+    sum = 0
+    for j in n:
+        sum += ord(j) - ord('0')
+    if isPrime(sum):
+        print("YES")
+    else:
+        print("NO")

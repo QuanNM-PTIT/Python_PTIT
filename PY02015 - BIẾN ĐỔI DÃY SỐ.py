1b4259673f32a3118a3b944e8a58b361307e4f0d
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-11-02 16:34:47 +0700

    Update

---
+++ PY02015 - BIẾN ĐỔI DÃY SỐ.py
@@ -0,0 +1,13 @@
+while True:
+    a = list(map(int, input().split()))
+    if a == [0, 0, 0, 0]:
+        break;
+    cnt = 0
+    while len(set(a)) > 1:
+        b = []
+        for i in range(3):
+            b.append(abs(a[i] - a[i + 1]))
+        b.append(abs(a[3] - a[0]))
+        a = b
+        cnt += 1
+    print(cnt)

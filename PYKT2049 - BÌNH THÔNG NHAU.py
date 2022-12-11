61f640de2748c607e04cb4ae182f25efe198935c
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2022-12-12 00:42:56 +0700

    Update

---
+++ PYKT2049 - BÌNH THÔNG NHAU.py
@@ -0,0 +1,29 @@
+root = [0 for i in range(100005)]
+
+def findRoot(x):
+    if x == root[x]:
+        return x
+    root[x] = findRoot(root[x])
+    return root[x]
+
+def check(x, y):
+    rootX = findRoot(x)
+    rootY = findRoot(y)
+    if rootX == rootY:
+        return 1
+    return 0
+
+def union(x, y):
+    rootX = findRoot(x)
+    rootY = findRoot(y)
+    root[rootX] = rootY
+
+for i in range(100005):
+    root[i] = i
+
+for i in range(int(input())):
+    x, y, z = map(int, input().split())
+    if z == 1:
+        union(x, y)
+    else:
+        print(check(x, y))

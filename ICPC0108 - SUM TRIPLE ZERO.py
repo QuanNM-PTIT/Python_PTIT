e22f61c30dfcfe30809a73495631a0fd38c49e12
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2022-12-13 01:35:52 +0700

    Update

---
+++ ICPC0108 - SUM TRIPLE ZERO.py
@@ -0,0 +1,18 @@
+for i in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    s = 0
+    for i in range(0, n - 2):
+        l = i + 1
+        r = len(a) - 1
+        x = a[i]
+        while l < r:
+            if x + a[l] + a[r] == 0:
+                s += 1
+                l += 1
+            elif x + a[l] + a[r] < 0:
+                l += 1
+            else:
+                r -= 1
+    print(s)
